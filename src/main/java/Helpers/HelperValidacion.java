3b7badf2b786a9cf7beba36a3b98a45896684f9f
SebasMunoz23 <smunozcarvajal234@gmail.com>
2024-04-07 17:13:57 -0500

    "se agregaron nuevas funciones pero hace flata la funcion RetornarCEV2Contraseña"

--- src/main/java/Helpers/HelperValidacion.java
+++ src/main/java/Helpers/HelperValidacion.java
@@ -72,9 +72,19 @@ public class HelperValidacion {
         }
         return Letra;
     }
-
+    
+    public static int ValidarCantidadRango(int cantidad)
+    {
+        if (cantidad>0 && cantidad<1000) {
+            return 1;
+        } else{
+             return 0;
+        }
+       
+    }
+    
     //Validar caracteres especiales en el campo direccion
-    public static int RetornarValorCE2(String Texto) {
+    public static int RetornarValorCEV2(String Texto) {
         int Caracter = 0;
         ArrayList<Character> Lista_caracter = new ArrayList<>();
 
@@ -107,11 +117,11 @@ public class HelperValidacion {
     }
 
     //Validar vacio
-    public static boolean ValidarVacio(String cadena) {
+    public static int ValidarVacio(String cadena) {
         if (cadena.equals("")) {
-            return true;
+            return 1;
         } else {
-            return false;
+            return 0;
         }
     }
     
@@ -140,5 +150,30 @@ public class HelperValidacion {
 
         return ce;
     }
+      
+      public static int ValidarTodoLetra (String cadena)
+      {
+          int conteo = ValidarVacio(cadena)+RetornarValorCEV2(cadena)+RetornarValorLetra(cadena);
+          return conteo;
+      }
+      
+       public static int ValidarTodoDireccion (String cadena)
+      {
+          int conteo = ValidarVacio(cadena)+RetornarCEDireccionV2(cadena);
+          return conteo;
+      }
+       
+        public static int ValidarTodoSerial (String cadena)
+      {
+          int conteo = ValidarVacio(cadena)+RetornarValorCEV2(cadena);
+          return conteo;
+      }
+         public static int ValidarTodoContraseña(String cadena)
+      {
+          int conteo = ValidarVacio(cadena);//falta la funcion RetornarCEVContraseña
+          return conteo;
+      }
+         
+      
 
 }
