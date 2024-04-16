08c42b96998083f35704c773e51143cbc4d4b4cc
juan diego <jgiraldominoz2006@gmail.com>
2024-04-16 10:25:09 -0500

    Se Coloco el metodo BuscarPersonNube en Helpers Impresion

--- src/main/java/Helpers/HelperImpresion.java
+++ src/main/java/Helpers/HelperImpresion.java
@@ -48,4 +48,50 @@ public class HelperImpresion {
         }
 
     }
+    
+    public static void BuscarPersonaNube(ArrayList<Persona> lspersonanube, String codigo) {
+        String[] partes = null,
+                partes2 = null;
+
+        int bandera = 0;
+        Producto objprodu;
+
+        ArrayList<Producto> lsnube = new ArrayList<>();
+
+        for (int i = 0; i < lspersonanube.size(); i++) {
+            if (codigo.equals(lspersonanube.get(i).getUid())) {
+                bandera = 1;
+                System.out.println("Persona" + "\t" + (i + 1) + "\n"
+                        + "El id de la persona es:" + lspersonanube.get(i).getUid() + "\n"
+                        + "El nombre de la persona es:" + "\t" + lspersonanube.get(i).getNombre() + "\n"
+                        + "El apellido de la persona es:" + "\t" + lspersonanube.get(i).getApellido() + "\n"
+                        + "La cedula de la persona es:" + "\t" + lspersonanube.get(i).getCedula() + "\n"
+                        + "La direccion de la persona es:" + "\t" + lspersonanube.get(i).getDireccion() + "\n"
+                );
+                partes = lspersonanube.get(i).getProducto().split(";");
+
+                for (int j = 0; j < partes.length; j++) {
+
+                    partes2 = partes[j].split(",");
+                    objprodu = new Producto(partes2[0], partes2[1], partes2[2]);
+                    lsnube.add(objprodu);
+
+                }
+                 for (int k = 0; k < lsnube.size(); k++) {
+                     System.out.println("Producto:"+"\t"+(k+1));
+                     System.out.println("Nombre"+"\t"+lsnube.get(k).getNombre());
+                     System.out.println("Marca"+"\t"+lsnube.get(k).getMarca());
+                     System.out.println("Serial"+"\t"+lsnube.get(k).getSerial()+"\n");
+                    
+}
+                 lsnube.clear();
+                 partes=null;
+                 partes2=null;
+            }
+        
+        }
+        if (bandera==0){
+            System.out.println("El uid buscando no se encuentra en la lista ");
+        }
+    }
 }
