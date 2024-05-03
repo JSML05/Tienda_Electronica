4abc6ceaa78eb009cb78f6da0c275798bcce1adb
SebasMunoz23 <smunozcarvajal234@gmail.com>
2024-05-03 09:52:44 -0500

    "Se agrego la imagen de fondo en inicio de sesion"

--- src/main/java/Helpers/HelperValidacion.java
+++ src/main/java/Helpers/HelperValidacion.java
@@ -44,7 +44,6 @@ public class HelperValidacion {
         Lista_caracter.add('"');
         Lista_caracter.add('!');
         Lista_caracter.add(' ');
-        
 
         for (int i = 0; i < Texto.length(); i++) {
             boolean flag = Character.isLetter(Texto.charAt(i));
@@ -72,17 +71,16 @@ public class HelperValidacion {
         }
         return Letra;
     }
-    
-    public static int ValidarCantidadRango(int cantidad)
-    {
-        if (cantidad>0 && cantidad<1000) {
+
+    public static int ValidarCantidadRango(int cantidad) {
+        if (cantidad > 0 && cantidad < 1000) {
             return 1;
-        } else{
-             return 0;
+        } else {
+            return 0;
         }
-       
+
     }
-    
+
     //Validar caracteres especiales en el campo direccion
     public static int RetornarValorCEV2(String Texto) {
         int Caracter = 0;
@@ -124,8 +122,8 @@ public class HelperValidacion {
             return 0;
         }
     }
-    
-      public static int RetornarCEDireccionV2(String nombre) {
+
+    public static int RetornarCEDireccionV2(String nombre) {
         int ce = 0;
         ArrayList<Character> Lista_caracter = new ArrayList<>();
 
@@ -150,30 +148,30 @@ public class HelperValidacion {
 
         return ce;
     }
-      
-      public static int ValidarTodoLetra (String cadena)
-      {
-          int conteo = ValidarVacio(cadena)+RetornarValorCEV2(cadena)+RetornarValorLetra(cadena);
-          return conteo;
-      }
-      
-       public static int ValidarTodoDireccion (String cadena)
-      {
-          int conteo = ValidarVacio(cadena)+RetornarCEDireccionV2(cadena);
-          return conteo;
-      }
-       
-        public static int ValidarTodoSerial (String cadena)
-      {
-          int conteo = ValidarVacio(cadena)+RetornarValorCEV2(cadena);
-          return conteo;
-      }
-         public static int ValidarTodoContraseña(String cadena)
-      {
-          int conteo = ValidarVacio(cadena);//falta la funcion RetornarCEVContraseña
-          return conteo;
-      }
-         
-      
+
+    public static int ValidarTodoLetra(String cadena) {
+        int conteo = ValidarVacio(cadena) + RetornarValorCEV2(cadena) + RetornarValorLetra(cadena);
+        return conteo;
+    }
+
+    public static int ValidarTodoDireccion(String cadena) {
+        int conteo = ValidarVacio(cadena) + RetornarCEDireccionV2(cadena);
+        return conteo;
+    }
+
+    public static int ValidarTodoSerial(String cadena) {
+        int conteo = ValidarVacio(cadena) + RetornarValorCEV2(cadena);
+        return conteo;
+    }
+
+    public static int ValidarTodoContraseña(String cadena) {
+        int conteo = ValidarVacio(cadena);//falta la funcion RetornarCEVContraseña
+        return conteo;
+    }
+
+    public static int ValidarTodo(String cadena) {
+        int conteo = ValidarVacio(cadena) + RetornaValor(cadena) + RetornarValorCEV2(cadena);
+        return conteo;
+    }
 
 }
